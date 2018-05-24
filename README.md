cece367cc5c3b2bc48dd658e103ad0dec4aefe3f
sanchezzzhak <sanchezzzhak@ya.ru>
2018-05-24 14:58:23 +0300

    update readme.md add example;

--- README.md
+++ README.md
@@ -5,17 +5,31 @@ port phplib piwik/device-detector from nodejs
 
 ### library works only under nodejs v10+
 
-### Usage
-
-```js
-const detector = new require('node-device-detector');
-console.dir(detector.findBrowser('user-agent'));
-```
+### Install
 local install production
 ```
 npm install node-device-detector --production
 ```
 local machine install is developer
 ```
-npm install --only=dev
+npm install node-device-detector --only=dev
+```
+
+### Usage
+
+```js
+const DeviceDetector = require('node-device-detector');
+
+let userAgent = 'Mozilla/5.0 (Linux; Android 5.0; NX505J Build/KVT49L) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.78 Mobile Safari/537.36';
+let detector = new DeviceDetector;
+
+console.log(detector.detect(userAgent));
+```
+### Result parse
+```json
+{ 
+    device: { brand: 'ZTE', model: 'Nubia Z7 max', type: 'smartphone' },
+    os: { name: 'Android', version: '5.0' },
+    browser: { name: 'Chrome Mobile', version: '43.0.2357.78' } 
+}
 ```
--- tests/parse-rtfc.js
+++ tests/parse-rtfc.js
@@ -1,11 +1,11 @@
 // User-Agent: Mozilla/<version> (<system-information>) <platform> (<platform-details>) <extensions>
-let regular = '/' + [
-    '(.*)?\s',  // version
-    '\((.*)\)', // system-information
-    '(.*)',     // platform
-    '\((.*)\)', // platform-details
-    '(.*)'      // extensions
-].join('') + '/i';
+// let regular = '/' + [
+//     '(.*)?\s',  // version
+//     '\((.*)\)', // system-information
+//     '(.*)',     // platform
+//     '\((.*)\)', // platform-details
+//     '(.*)'      // extensions
+// ].join('') + '/i';
 
 
 // схема оптимизации
@@ -14,3 +14,11 @@ let regular = '/' + [
 // 1 парсим user-agent на состовляющие
 // 2 проверяем pull_array есть информация, если нету информации парсим через все регулярки и создаем хеш.
 // 4 выгрузить из статистики user-agents с 2015-2018 годы для фикстур
+
+const DeviceDetector = require('./../index');
+
+let userAgent = 'Mozilla/5.0 (Linux; Android 5.0; NX505J Build/KVT49L) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.78 Mobile Safari/537.36';
+let detector = new DeviceDetector;
+let result = detector.detect(userAgent);
+
+console.log(result);
