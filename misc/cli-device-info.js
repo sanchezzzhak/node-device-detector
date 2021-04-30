c3006d5a1754c1087b0bed1f8ba1a64eb317afbf
sanchezzzhak <sanchezzzhak@ya.ru>
2021-04-30 17:02:07 +0300

    feat(device-info) added device info for brands: Asus

--- misc/cli-device-info.js
+++ misc/cli-device-info.js
@@ -1,19 +1,37 @@
 const DeviceInfo = require('../parser/device/info-device');
+const YAML = require('js-yaml');
+
 const deviceInfo = new DeviceInfo();
 deviceInfo.setResolutionConvertObject(true);
 deviceInfo.setSizeConvertObject(true);
 
+const REPORT_TYPE_YML = 'yml';
+const REPORT_TYPE_USERAGENT = 'useragent';
+const REPORT_TYPE_JSON = 'json';
+
 let brand = process.argv[2];
 let model = process.argv[3];
+let format = process.argv[4] || 'yml';
 
-console.log(
-  JSON.stringify(
-    {
-      brand: brand,
-      model: model,
-      result: deviceInfo.info(brand, model),
-    },
-    null,
-    2
-  )
-);
+function printReport(result, format = 'yml') {
+  if (REPORT_TYPE_YML === format) {
+    console.log(YAML.dump([result], { indent: 2, lineWidth: Infinity }));
+    return;
+  }
+  if (REPORT_TYPE_USERAGENT === format) {
+    console.log(result.user_agent);
+    return;
+  }
+  if (REPORT_TYPE_JSON === format) {
+    console.log(result);
+    return;
+  }
+}
+
+let result = {
+  brand: brand,
+  model: model,
+  result: deviceInfo.info(brand, model),
+};
+
+printReport(result, format);
