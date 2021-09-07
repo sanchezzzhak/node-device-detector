8bbe5661e4845eaea7c6a7a52967ee65e10f1c8d
sanchezzzhak <sanchezzzhak@ya.ru>
2021-09-07 09:23:28 +0300

    feat(misc) added detect bot for misc/cli.js

--- misc/cli.js
+++ misc/cli.js
@@ -1,8 +1,8 @@
 const readline = require('readline');
 const DeviceDetector = require('../index');
-const detector = new DeviceDetector({ skipBotDetection: false });
 const YAML = require('js-yaml');
 
+const detector = new DeviceDetector;
 const formats = ['json', 'yml'];
 const format =
   process.argv[3] !== void 0 && formats.indexOf(process.argv[3]) !== -1
@@ -12,40 +12,50 @@ const format =
 const print = process.argv[4] || 'no';
 
 function parse(useragent) {
-  let result = { user_agent: useragent };
-  result = Object.assign(result, detector.detect(useragent));
-
-  if (print === 'fixture') {
-    let osFamily = result.os.family;
-    let clientFamily = result.client.family;
-    
-    result.client && result.client.short_name && delete result.client.short_name;
-    result.client && result.client.family && delete result.client.family;
-    result.os && result.os.short_name && delete result.os.short_name;
-    result.os && result.os.family && delete result.os.family;
-    result.device && result.device.id && delete result.device.id;
-    
-    result['os_family'] = osFamily;
-    result['browser_family'] = clientFamily;
+  let result = {user_agent: useragent};
+  let botResult = detector.parseBot(useragent);
+  if (botResult && botResult.name) {
+    result = Object.assign(result, botResult);
+  } else {
+    result = Object.assign(result, detector.detect(useragent));
   }
-
+  
+  if (print === 'fixture') {
+    if (result.client) {
+      let osFamily = result.os.family;
+      let clientFamily = result.client.family;
+      result.client && result.client.short_name &&
+      delete result.client.short_name;
+      result.client && result.client.family && delete result.client.family;
+      result.os && result.os.short_name && delete result.os.short_name;
+      result.os && result.os.family && delete result.os.family;
+      result.device && result.device.id && delete result.device.id;
+      
+      result['os_family'] = osFamily;
+      result['browser_family'] = clientFamily;
+    }
+  }
+  
   if (format === 'yml') {
-    console.log(YAML.dump([result], { indent: 2, lineWidth: Infinity }));
+    console.log(YAML.dump([result], {indent: 2, lineWidth: Infinity}));
+    return;
   }
   if (format === 'json') {
     console.log(result);
+    return;
   }
 }
-
 if (process.argv.length > 2) {
   parse(process.argv[2]);
-} else {
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
-  rl.question('user agent:', (userAgent) => {
-    parse(userAgent);
-    rl.close();
-  });
+  return;
 }
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question('user agent: ', (userAgent) => {
+  parse(userAgent);
+  rl.close();
+});
