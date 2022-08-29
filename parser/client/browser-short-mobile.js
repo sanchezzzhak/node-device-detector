6de664b1c2ebdeb95459cca14c76f8e23645cf49
Tutik Alexsandr <sanchezzzhak@ya.ru>
2022-08-30 01:56:13 +0300

    Pr device info65 (#106)
    * Adds detection for various apps (matomo-org#7221)
    * Adds detection for iSafePlay
    * Improves detection for Baidu Box App
    * Adds detection for T2S
    * Adds detection for Photo Sherlock
    * Adds detection for Reverse Image Search
    * Adds detection for Joy Launcher
    * Adds detection for TCL Live
    * Adds detection for Social Media Explorer
    * Adds detection for Reddit
    * Adds detection for Omshy VPN
    * Adds detection for Opera News
    * Adds detection for Search By Image
    * Adds detection for ShareKaro
    * Adds detection for NewsPoint
    * Adds detection for XOS Launcher
    * Adds detection for HiOS Launcher
    * Adds detection for itel Launcher
    * Adds detection for Twitter
    * Adds detection for Waze
    * Adds detection for Tuya Smart Life
    * Adds detection for Clean Master
    * Adds detection for TV Cast
    * Adds detection for CM Security
    * Adds detection for IDM Video Download Manager
    * Adds detection for Coupons & Deals
    * Adds detection for Quick Cast* Adds detection for Download Manager
    
    Adds detection for various browsers (matomo-org#7222)
    
    * Improves detection for Ume Browser
    * Improves detection for Opera Mini
    * Adds detection for TUC Mini Browser
    * Adds detection for AppBrowzer
    * Adds detection for SX Browser
    * Adds detection for Fiery Browser
    * Adds detection for YAGI
    * Adds detection for APN Browser
    * Improves detection for QQ Browser
    * Adds detection for NextWord Browser
    * Improves detection for Naked Browser
    * Adds detection for Naked Browser Pro
    * Adds detection for 1DM Browser
    * Adds detection for 1DM+ Browser
    * Adds detection for Adult Browser
    * Adds detection for XNX Browser
    * Adds detection for XtremeCast
    * Adds detection for X Browser Lite
    * Adds detection for xBrowser
    * Improves detection for Open Browser
    * Adds detection for Sweet Browser
    * Improves detection for mCent
    * Adds detection for HTC Browser
    
    Detect devices for existing brands and new brands (#matomo-org7224)
    feat(device) detect new brand Unblock Tech: S900 Pro BT, S900 Pro
    feat(device) detect brand GoMobile: Go506
    feat(device) detect brand FiGi: Gtwo
    feat(device) detect brand HiKing: MH501
    feat(device) detect brand Advance: Hollogram HL7257
    feat(device) detect brand FireFly Mobile: S90 Q
    feat(device) detect new brand C5 Mobile: Noa X2 Plus, Noa X Power
    feat(device) detect brand Selfix: Q3 Rebel, Q6 Stallion, V41 Pro Mardigras
    feat(device) detect brand Leagoo: Shark 1, Shark 6300
    feat(device) detect brand Obi: SJ1.5, SJ2.2, SJ3.2
    feat(device) detect brand NOA: Hummer
    feat(device) detect new brand Chainway: C66
    feat(device) detect brand LT Mobile: P20 (C3600), P30 (C3700)
    feat(device) detect brand DEXP: B11
    feat(device) detect brand Tecno Mobile: Spark 9 Pro (TECNO KH7n)
    feat(device) detect brand Wiko: Y62 Plus
    feat(device) detect brand AGM: H5 Pro
    feat(device) detect brand OPPO: Reno 8 Pro (CPH2359)
    feat(device) detect brand Xiaomi: Redmi K50i (22041216I)
    feat(device) detect brand DEXP: C37
    feat(device) detect new brand Harper: ABX-332
    feat(device) detect brand Huawei: P50 (ABR-LX9)
    feat(device) detect brand M-Tech: Foto 3
    feat(device) detect brand Hurricane: Flint
    feat(device) detect brand Vivax: Fly 4
    feat(device) detect brand HiKing: Fun 5
    feat(device) detect brand FiGi: Ftwo
    feat(device) detect brand SPC: Flow 7
    feat(device) detect brand Quantum: Go 2
    feat(device) detect brand Blu: Grand M2 LTE, Grand M2, Grand M2X, Grand XL
    feat(device) detect brand Zuum: Grant
    feat(device) detect brand SPC: Gravity 3G, Gravity Pro
    
    * chore: update device-index-hash.yml

--- parser/client/browser-short-mobile.js
+++ parser/client/browser-short-mobile.js
@@ -6,10 +6,11 @@ module.exports = [
   'MN', 'MZ', 'NX', 'OC', 'OI', 'OM', 'OZ', 'PU', 'PI',
   'PE', 'QU', 'RE', 'S0', 'S7', 'SA', 'SB', 'SG', 'SK',
   'ST', 'SU', 'T1', 'UH', 'UM', 'UT', 'VE', 'VV', 'WI',
-  'WP', 'YN', 'IO', 'IS', 'HQ', 'RW', 'HI', 'NA', 'BW',
+  'WP', 'YN', 'IO', 'IS', 'HQ', 'RW', 'HI', 'PN', 'BW',
   'YO', 'PK', 'MR', 'AP', 'AK', 'UI', 'SD', 'VN', '4S',
   'RF', 'LR', 'SQ', 'BV', 'L1', 'F0', 'KS', 'V0', 'C8',
   'AZ', 'MM', 'BT', 'N0', 'P0', 'F3', 'DU', 'D0', 'P1',
   'O4', 'XO', 'U0', 'B0', 'VA', 'X0', 'A5', 'X1', '18',
-  'B5', 'B6',
+  'B5', 'B6', 'TC', 'A6', '2X', 'F4', 'YG', 'WR', 'NA',
+  'DM', '1M', 'A7', 'XN', 'XT', 'XB', 'W1', 'HT',
 ];
