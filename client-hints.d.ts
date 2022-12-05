621972735b30cb682503ced120b17477e0e05910
Awaludin AR <34894621+AwaludinAR@users.noreply.github.com>
2022-12-05 14:52:58 +0700

    Typescript support: Add client-hints.d.ts and modify index.d.ts (#122)
    * Create client-hints.d.ts
    * Update index.d.ts
    * Update README.md

---
+++ client-hints.d.ts
@@ -0,0 +1,13 @@
+export default class ClientHints {
+  static getHeaderClientHints(): JSONObject;
+  static isSupport(headers: JSONObject): boolean;
+
+  parse(objHeaders: JSONObject): JSONObject;
+}
+
+export type JSONValue = string | number | boolean | JSONObject | JSONArray;
+
+export interface JSONObject {
+  [k: string]: JSONValue;
+}
+export type JSONArray = Array<JSONValue>;
