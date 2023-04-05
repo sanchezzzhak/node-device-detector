15f89f358d3ba65972c2d720dee9c076d7897781
sanchezzzhak <sanchezzzhak@ya.ru>
2023-04-05 17:03:01 +0300

    Detect new brand HIPER and Detect devices for exits brands: Samsung, Xiaomi, BBK, SUNWIND, Stylo (matomo-org#7382) feat(device) detect brand Tecno Mobile: Spark Go (2023) (TECNO BF7n), Camon X Pro (TECNO CA8S) feat(device) detect brand Samsung: Galaxy A14 (SM-A145P) feat(device) detect brand Xiaomi: Redmi 9C NFC (angelican) feat(device) detect brand UNIWA: W888 feat(device) detect brand iTel: A60 (A662L) feat(device) detect brand Land Rover: ST11-A5 feat(device) detect brand Teclast: M40 Pro feat(device) detect brand Vonino: Magnet M10 (2020) feat(device) detect brand Vankyo: MatrixPad Z1 feat(client) detect apps: My Watch Party, Zalo feat(device) exclude android os for iphone regex feat(device) detect new brand HIPER: Media Stick 4K, Media TV 6K, Media TV i4 Pro, Media TV i4 feat(device) detect brand Xiaomi: Redmi 12C (22126RN91Y), Redmi Note 10 5G (vnd_camellia) feat(device) detect brand Coolpad: Cool 20 Pro (CP05) feat(device) detect brand Gionee: F8 Neo feat(device) detect brand Stylo: Orion 8 Tab feat(device) detect brand IconBIT: Movie X feat(device) detect brand SUNWIND: Sky 8244B 3G, Sky 1430D 4G, Sky 1123B 3G, Sky 1262C 3G feat(device) detect brand Samsung: Galaxy W20 (SM-W2020), Galaxy A12 (SM-A125V), Galaxy Tab S7 FE 12.4" 5G (SM-T738T, SM-T738V), Galaxy Tab S7 FE 12.4" LTE (SM-T735C) feat(device) detect brand BBK: 40LEX-7257/FTS2C, 40LEX-7259/FTS2C, 43LEX-8243/UTS2C chore: update device and client indexes

--- parser/device/alias-device.js
+++ parser/device/alias-device.js
@@ -7,17 +7,28 @@ const createReplaceBrandRegexp = () => {
   let escapeeChars = [/\+/gi, /\./gi];
   let replaceChars = ['\\+', '\\.'];
   let customBrands = ['HUAWEI HUAWEI', 'viv-vivo'];
-  let brands = customBrands
-  .concat(Object.keys(COLLECTION_BRAND_LIST))
-  .join('|');
+  let brands = customBrands.concat(Object.keys(COLLECTION_BRAND_LIST)).
+  join('|');
   for (let i = 0, l = escapeeChars.length; i < l; i++) {
     brands = brands.replace(escapeeChars[i], replaceChars[i]);
   }
   return new RegExp(
-    '(?:^|[^A-Z0-9-_]|[^A-Z0-9-]_|sprd-)(' + brands + ')[ _]',
-    'isg'
+      '(?:^|[^A-Z0-9-_]|[^A-Z0-9-]_|sprd-)(' + brands + ')[ _]',
+      'isg',
   );
+};
+
+const normalizationUserAgent = (userAgent) => {
+  let rawUA = String(userAgent)
+  if (
+      /;ip(?:ad|hone): build\//i.test(rawUA) &&
+      /android /i.test(rawUA)
+  ) {
+    rawUA = rawUA.replace(/;ip(?:ad|hone):/i, '')
+  }
+  return rawUA
 }
+
 const REPLACE_BRAND_REGEXP = createReplaceBrandRegexp();
 
 class AliasDevice extends AbstractParser {
@@ -36,20 +47,20 @@ class AliasDevice extends AbstractParser {
     this.__replaceBrand = replace;
   }
 
-
   /**
    * @param {string} userAgent
    * @returns {{name: string}}
    */
   parse(userAgent) {
     userAgent = this.prepareUserAgent(userAgent);
+    userAgent = normalizationUserAgent(userAgent);
     let result = {
       name: '',
     };
     let decodeUserAgent = '';
     let isDecodeUA = /%[2-4][0-6A-F]/i.test(userAgent);
     try {
-      decodeUserAgent = isDecodeUA ? decodeURIComponent(userAgent) : userAgent
+      decodeUserAgent = isDecodeUA ? decodeURIComponent(userAgent) : userAgent;
     } catch (err) {}
 
     for (let cursor in this.collection) {
@@ -58,8 +69,7 @@ class AliasDevice extends AbstractParser {
       if (match) {
         result.name = this.buildByMatch(item['name'], match);
         if (this.hasReplaceBrand()) {
-          result.name = result.name
-            .replace(REPLACE_BRAND_REGEXP, '')
+          result.name = result.name.replace(REPLACE_BRAND_REGEXP, '');
         }
         break;
       }
@@ -67,7 +77,7 @@ class AliasDevice extends AbstractParser {
     result.name = result.name.trim();
     return result;
   }
-  
+
 }
 
 module.exports = AliasDevice;
